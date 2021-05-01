fd0a485f8df187b5ddcd3b351ac3e9670a507819
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 22:43:35 -0700

    Update

--- empWageV2.sh
+++ empWageV2.sh
@@ -1,11 +1,15 @@
-#! /bin/bash -x
+#! /bin/bash
+
 
 #Constants
 IS_PRESENT=1
+EMP_RATE_PER_HR=20
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_HALF_TIME=2
 
 #Variables
 empCheck=$(( RANDOM%2 ))
-
+empCheck1=$(( RANDOM%3 ))
 #Selection
 if [ $empCheck -eq $IS_PRESENT ]
 then
@@ -13,38 +17,24 @@ then
 else
     echo "Employee is absent"
 fi
-#Constants
-IS_PRESENT=1
-EMP_RATE_PER_HR=20
-
-#Variables
-empCheck=$(( RANDOM%2 ))
-
 #Selection
 if [ $empCheck -eq $IS_PRESENT ]
 then 
       empHrs=8
 else 
       empHrs=0
-fi 
+fi  
 salary=$(( EMP_RATE_PER_HR * empHrs ))
-#Constants
-IS_PRESENT_FULL_TIME=1
-IS_PRESENT_HALF_TIME=2
-EMP_RATE_PER_HR=20
-
-#Variables
-empCheck=$(( RANDOM%3 ))
-
+echo $salary
 #Selection
-if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
+if [ $empCheck1 -eq $IS_PRESENT_FULL_TIME ]
 then
       empHrs=8
-elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
+elif [ $empCheck1 -eq $IS_PRESENT_HALF_TIME ]
 then
       empHrs=4
 else
       empHrs=0
 fi
 salary=$(( EMP_RATE_PER_HR * empHrs ))
-
+echo $salary
