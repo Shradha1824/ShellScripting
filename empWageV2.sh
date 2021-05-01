792ae35d8a82d513fb8a9f15b312cb4b9605e5c4
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 23:45:17 -0700

    Updated file adding with calculate wage for month

--- empWageV2.sh
+++ empWageV2.sh
@@ -6,10 +6,14 @@ IS_PRESENT=1
 EMP_RATE_PER_HR=20
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=2
+MAX_WORKING_DAYS=20
 
 #Variables
 empCheck=$(( RANDOM%2 ))
 empCheck1=$(( RANDOM%3 ))
+totalworkingHours=0
+totalworkingDays=0
+
 #Selection
 if [ $empCheck -eq $IS_PRESENT ]
 then
@@ -44,3 +48,19 @@ case $empCheck1 in
        *) empHrs=0;;
 esac
 salary=$(( EMP_RATE_PER_HR * empHrs ))
+while [ $totalworkingDays -ne $MAX_WORKING_DAYS ]
+do
+       employeeCheck=$((RANDOM%3))
+       ((totalworkingDays++))
+       case $empCheck1 in
+              $IS_PRESENT_FULL_TIME)
+                     empHrs=8 ;;
+              $IS_PRESENT_HALF_TIME)
+                     empHrs=4 ;;
+                                  *)
+                     empHrs=0 ;;
+       esac
+       totalworkingHours=$(( totalworkingHours + empHrs ))
+done
+salary=$(( totalworkingHours * WAGE_PER_HR ))
+
