ab92f4937a793520f8c79c5aa18c17fceb65576f
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 21:53:41 -0700

    ADDED:empWageV2.sh file

---
+++ empWageV2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash -x
+
+#Constants
+IS_PRESENT=1
+EMP_RATE_PER_HR=20
+
+#Variables
+empCheck=$(( RANDOM%2 ))
+
+#Selection
+if [ $empCheck -eq $IS_PRESENT ]
+then 
+      empHrs=8
+else 
+      empHrs=0
+fi 
+salary=$(( EMP_RATE_PER_HR * empHrs ))
