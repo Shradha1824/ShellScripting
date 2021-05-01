099adf2bc52d78257db459348e9e76f9006719f5
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 22:11:24 -0700

    Modified:empWageV2.sh

--- empWageV2.sh
+++ empWageV2.sh
@@ -2,6 +2,19 @@
 
 #Constants
 IS_PRESENT=1
+
+#Variables
+empCheck=$(( RANDOM%2 ))
+
+#Selection
+if [ $empCheck -eq $IS_PRESENT ]
+then
+    echo "Employee is present"
+else
+    echo "Employee is absent"
+fi
+#Constants
+IS_PRESENT=1
 EMP_RATE_PER_HR=20
 
 #Variables
