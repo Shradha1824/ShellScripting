fe467a3216b314d19b2c00900bf84371d3b69b3e
Shradha1824 <shradhabj1824@gmail.com>
2021-04-30 22:30:56 -0700

    Remove extra constant

--- empWageV2.sh
+++ empWageV2.sh
@@ -2,6 +2,7 @@
 
 #Constants
 IS_PRESENT=1
+EMP_RATE_PER_HR=20
 
 #Variables
 empCheck=$(( RANDOM%2 ))
@@ -13,13 +14,6 @@ then
 else
     echo "Employee is absent"
 fi
-#Constants
-IS_PRESENT=1
-EMP_RATE_PER_HR=20
-
-#Variables
-empCheck=$(( RANDOM%2 ))
-
 #Selection
 if [ $empCheck -eq $IS_PRESENT ]
 then 
